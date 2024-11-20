4d799c5fb767f675aeae51febaee07578eb40d67
ogabrieloliver <contact.ogabrielpro@gmail.com>
2024-11-20 14:43:16 -0300

    Initial Commit

---
+++ Gasosa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gasosa
+//
+//  Created by Gabriel De Oliveira on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
